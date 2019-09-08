bb94cef962a3ab57e986089c560bd11ba4abbe33
Avhijit Dutta <counter.jit@gmail.com>
2019-09-08 18:09:55 +0530

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, ViewChildren, ElementRef } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'otp';
+  form: FormGroup;
+  formInput = ['input1', 'input2', 'input3', 'input4', 'input5', 'input6'];
+  @ViewChildren('formRow') rows: any;
+
+  ngOnInit() {
+
+  }
+
+  constructor() {
+    this.form = this.toFormGroup(this.formInput);
+  }
+
+  toFormGroup(elements) {
+    const group: any = {};
+
+    elements.forEach(key => {
+      group[key] = new FormControl('', Validators.required);
+    });
+    return new FormGroup(group);
+  }
+
+  keyUpEvent(event, index) {
+    let pos = index;
+    if (event.keyCode === 8 && event.which === 8) {
+      pos = index - 1 ;
+    } else {
+      pos = index + 1 ;
+    }
+    if (pos > -1 && pos < this.formInput.length ) {
+      this.rows._results[pos].nativeElement.focus();
+    }
+
+  }
+
+  onSubmit() {
+    console.log(this.form.value);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule} from '@angular/forms';
+import { FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +9,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
